0aa41c45ad192f0103d9598e15f2ea4d57c9b011
thefelling <vincent.ssb88@gmail.com>
2026-08-18 19:01:42 +0700

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,6 +15,12 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
-  }
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  publicDir: 'public' // ← PASTIKAN INI ADA (default)
 });
