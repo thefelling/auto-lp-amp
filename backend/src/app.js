c0e5c408bd701134ad3b23010492e1396f148582
thefelling <vincent.ssb88@gmail.com>
2026-08-19 19:16:01 +0700

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -45,7 +45,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: '*',
+  origin: '*', // ← SEMENTARA PAKE * DULU
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
